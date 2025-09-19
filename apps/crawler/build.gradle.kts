02a021f6f21563e95a1056cd96a2de73bfac2ed2
DA-RAE <styd4957@gmail.com>
2025-09-19 16:27:36 +0900

    [feature] 의존성 추가

--- apps/crawler/build.gradle.kts
+++ apps/crawler/build.gradle.kts
@@ -20,7 +20,13 @@ repositories {
 
 dependencies {
     implementation("org.springframework.boot:spring-boot-starter")
+    implementation("org.springframework.boot:spring-boot-starter-web")
+    implementation("org.springframework.boot:spring-boot-starter-batch")
+    implementation("org.springframework.boot:spring-boot-starter-data-jpa")
+    implementation("org.projectlombok:lombok")
     testImplementation("org.springframework.boot:spring-boot-starter-test")
+    testImplementation("org.springframework.batch:spring-batch-test")
+    "developmentOnly"("org.springframework.boot:spring-boot-devtools")
     testRuntimeOnly("org.junit.platform:junit-platform-launcher")
 }
 
